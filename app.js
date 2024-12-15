fd5b20d58904eaaa7c041582c3c375841fb9ce6c
shahzad866 <shahzad.pso@gmail.com>
2024-12-15 21:05:11 +0500

    initail commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var Name = "Ahmed Ali"
+
+console.log(Name)
+
+
+// Data TYPEs
+
+// var type1: string = "AHmed Ali"
+var type1 = "AHmed Ali"
+var type2 = 123
+var type3 = true
+var type4 = undefined
+var type5 = null
+
+// CAsees
+var camelCase = "null"
+var snake_case = "null"
+var PaskalCase = "null"
+var KABAB_CASE = "null"
+
+//Not Declear
+var name2;
+
+// Declear
+var name3 = "arsalan";
+console.log(type3)
+
+alert("Hello World")
